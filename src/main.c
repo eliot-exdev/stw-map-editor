d620d8090724126e7abac4cd11d6331d22817a7c
Andre Geisler <andre@exdev.de>
2025-12-06 12:41:06 +0100

    implemented loading tile map

--- src/main.c
+++ src/main.c
@@ -2,6 +2,25 @@
  * Copyright 2025 Andre Geisler (andre@exdev.de)
  */
 
+#include <exdevgfx/exdev_base.h>
+#include <exdevgfx/framebuffer_8bit.h>
+
 int main(void) {
+    exdev_base_init();
+
+    // build tile map
+    Framebuffer8Bit_t fb;
+    framebuffer_8bit_read_from_dat(&fb, "assets/maptiles_8bit.dat");
+    Tiles8bit_t tiles;
+    tiles_8bit_init_from_framebuffer(&tiles, &fb, 16, 16);
+    framebuffer_8bit_deinit(&fb);
+
+    Palette8Bit_t palette;
+    palette_8bit_read_from_dat(&palette, "assets/maptiles_8bit.pal");
+
+    // cleanup
+    tiles_8bit_deinit(&tiles);
+    exdev_base_deinit();
+
     return 0;
 }
